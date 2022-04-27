c723944009f0d6e4e667c9c0841123efb1771073
Kevin Hung <kevincjhung@gmail.com>
2022-04-26 21:29:14 -0700

    .

--- routes/shop_setup_router.js
+++ routes/shop_setup_router.js
@@ -7,6 +7,23 @@ router.get("/a", (req, res) => {
     res.render("index")
 })
 
+// GET /shop_setUp/shop_setUp_5
+router.get("/shop_setUp_5", (req, res) => {
+    res.render("shopSetUp/shopSetUp_5", {
+
+    })
+})
+
+router.get("/shop_setUp_6", (req, res) => {
+    
+})
+
+
+router.get("/shop_setUp_7", (req, res) => {
+    
+})
+
+
 
 
 
