7b064f849ec395c61befb123545c06145876e2fa
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2022-04-27 13:47:40 -0700

    Buttons color+ info
    testing if info is getting pass to the back from buttons

--- routes/shop_setup_router.js
+++ routes/shop_setup_router.js
@@ -7,7 +7,18 @@ router.get("/a", (req, res) => {
     res.render("index")
 })
 
+router.get("/shop_setup_4", (req, res) => {
+    res.render("shop_setup/shop_setup_4", {
+
+    })
+})
 
 
+router.post('/product_type', (req, res) => {
 
-module.exports = router;
+    let data = req.body.productTypeList
+    console.log("backEnd: ", data)
+    res.send(data)
+})
+
+    module.exports = router;
