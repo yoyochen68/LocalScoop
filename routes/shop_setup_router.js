1a186d29afe3a857f72f165cb39b01aabfedf531
Kevin Hung <kevincjhung@gmail.com>
2022-04-26 21:50:44 -0700

    .

--- routes/shop_setup_router.js
+++ routes/shop_setup_router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../fake-db");
+// path is for app.use(express.static())
 
 // GET /shop_setup/a
 router.get("/a", (req, res) => {
@@ -9,18 +10,20 @@ router.get("/a", (req, res) => {
 
 // GET /shop_setUp/shop_setUp_5
 router.get("/shop_setUp_5", (req, res) => {
-    res.render("shopSetUp/shopSetUp_5", {
+    res.render("shopSetUp/shopSetUp_5")
+})
+
+router.get("/shop_setUp_6", (req, res) => {
+    res.render("shopSetUp/shopSetUp_6", {
 
     })
 })
 
-router.get("/shop_setUp_6", (req, res) => {
-    
-})
-
 
 router.get("/shop_setUp_7", (req, res) => {
-    
+    res.render("shopSetUp/shopSetUp_7", {
+
+    })
 })
 
 
