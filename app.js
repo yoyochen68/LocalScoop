43a28e5512c47ac66d3bea1a628c98992dec69ce
Kevin Hung <kevincjhung@gmail.com>
2022-05-02 16:45:11 -0700

    .

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ const crypto = require('crypto')
 // fake-database
 const db = require("./fake-db")
 
-// other files çç
+// other files 
 const server = require("./server.js")
 
 // router files. require the router js files
-
 const shopSetupRouter = require("./routes/shop_setup_router")
 const productPostRouter = require("./routes/product_post_router")
+const ordersRouter = require("./routes/orders_router")
 
 
 
@@ -48,6 +48,8 @@ app.use(cookieSession({
 /**   router routes, set beginning of path   **/
 app.use("/shop_setup", shopSetupRouter);
 app.use("/product_post", productPostRouter);
+app.use("/orders", ordersRouter);
+
 
 // app.use("/product_post", productPostRouter);
 
