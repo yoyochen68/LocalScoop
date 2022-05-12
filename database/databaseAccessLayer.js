b1719617018c6b160fb111efd260cc1d4fbe9f67
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2022-05-12 15:19:42 -0700

    updating the code

--- database/databaseAccessLayer.js
+++ database/databaseAccessLayer.js
@@ -18,15 +18,15 @@ let database;
 // YASMINA's localHost
 
 /* change this so it matches yours */
-// const dbConfigLocal = {
-// 	host: "localhost",
-// 	user: "root",
-// 	password: "Fswd2021$",
-// 	database: "localscoop",
-// 	port: 3306,
-// 	multipleStatements: false,
-// 	namedPlaceholders: true
-// };
+const dbConfigLocal = {
+	host: "localhost",
+	user: "root",
+	password: "Fswd2021$",
+	database: "localscoop",
+	port: 3306,
+	multipleStatements: false,
+	namedPlaceholders: true
+};
 
 
 // KEVIN's localHost
@@ -45,15 +45,15 @@ let database;
 
 
 // YOYO local database
-const dbConfigLocal = {
-    host: "localhost",
-    user: "root",
-    password: "Password",
-    database: "localscoop_local",
-    port: 3306,
-    multipleStatements: false,
-    namedPlaceholders: true
-};
+// const dbConfigLocal = {
+//     host: "localhost",
+//     user: "root",
+//     password: "Password",
+//     database: "localscoop_local",
+//     port: 3306,
+//     multipleStatements: false,
+//     namedPlaceholders: true
+// };
 
 if (is_heroku) {
     database = mysql.createPool(dbConfigHeroku).promise();
