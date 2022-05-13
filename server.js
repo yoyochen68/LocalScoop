af0558d2cf0b6b45d4b314f03f04a10be79530e3
Kevin Hung <kevincjhung@gmail.com>
2022-05-13 13:07:35 -0700

    ..

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app")
-const mysql = require("mysql2")
-
-
-const PORT = process.env.PORT || 8000; // let express set PORT, else make it 8000
-
-app.listen(PORT, () => console.log(`server should be running at http://localhost:${PORT}/`))
-
-
