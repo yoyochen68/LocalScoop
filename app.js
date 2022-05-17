57503497bc529ccb4a68c6916c743900e3bb35d5
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2022-05-16 20:11:04 -0700

    interface

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ const addCartRouter = require("./routes/add_cart_router")
 const shoppingCartRouter = require("./routes/shopping_cart_router")
 const followBusinessRouter = require("./routes/follow_business_router")
 const buyerSetupRouter = require("./routes/buyer_setup_router")
+const chatRouter = require("./routes/chat_router")
 
 // const sellerHomeRouter = require("./routes/seller_home_router")
 
@@ -75,6 +76,8 @@ app.use("/add_cart", addCartRouter)
 app.use("/shopping_cart", shoppingCartRouter)
 app.use("/follow_business", followBusinessRouter)
 app.use("/buyer_setup", buyerSetupRouter)
+app.use("/chat", chatRouter)
+
 
 
 
---
+++ routes/chat_router.js
@@ -0,0 +1,96 @@
+/* libraries */
+const express = require("express");
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+const crypto = require('crypto')
+const router = express.Router();
+const mysqlDB = require('../database/databaseAccessLayer')
+
+
+
+
+
+//all products
+// router.get("/products",  async (req, res) => {
+//   res.render("add_cart/products", {productInfo:productInfo, cartItemsTotal:cartItemsTotal })
+//
+// })
+
+
+
+const http = require('http');
+const socketio = require('socket.io');
+// const formatMessage = require('./utils/messages');
+// const {
+//   userJoin,
+//   getCurrentUser,
+//   userLeave,
+//   getRoomUsers
+// } = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+// const botName = 'ChatCord Bot';
+router.get("/",  async (req, res) => {
+  res.render("chat/test")
+
+})
+
+
+// Run when client connects
+// io.on('connection', socket => {
+//   socket.on('joinRoom', ({ username, room }) => {
+//     const user = userJoin(socket.id, username, room);
+//
+//     socket.join(user.room);
+//
+//     // Welcome current user
+//     socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+//
+//     // Broadcast when a user connects
+//     socket.broadcast
+//         .to(user.room)
+//         .emit(
+//             'message',
+//             formatMessage(botName, `${user.username} has joined the chat`)
+//         );
+//
+//     // Send users and room info
+//     io.to(user.room).emit('roomUsers', {
+//       room: user.room,
+//       users: getRoomUsers(user.room)
+//     });
+//   });
+//
+//   // Listen for chatMessage
+//   socket.on('chatMessage', msg => {
+//     const user = getCurrentUser(socket.id);
+//
+//     io.to(user.room).emit('message', formatMessage(user.username, msg));
+//   });
+//
+//   // Runs when client disconnects
+//   socket.on('disconnect', () => {
+//     const user = userLeave(socket.id);
+//
+//     if (user) {
+//       io.to(user.room).emit(
+//           'message',
+//           formatMessage(botName, `${user.username} has left the chat`)
+//       );
+//
+//       // Send users and room info
+//       io.to(user.room).emit('roomUsers', {
+//         room: user.room,
+//         users: getRoomUsers(user.room)
+//       });
+//     }
+//   });
+// });
+
+module.exports = router;
