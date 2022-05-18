90f99c2995c469d262919525d34fd0d163da6b55
Kevin Hung <kevincjhung@gmail.com>
2022-05-17 17:03:59 -0700

    formatting

--- routes/follow_business_router.js
+++ routes/follow_business_router.js
@@ -10,7 +10,6 @@ const mysqlDB = require('../database/databaseAccessLayer')
 const { append } = require("express/lib/response");
 
 
-
 // GET /follow_business/follow_business_1
 router.get("/follow_business_1", async(req, res) => {
     let cartItemsTotal = 0
@@ -34,34 +33,22 @@ router.get("/follow_business_1", async(req, res) => {
 })
 
 
-
-
 // GET /follow_business/follow_business_2
-
 router.get("/follow_business_2/:id", async (req, res) => {
 
     let storeId = req.params.id
-
-
     let storeInfo = await mysqlDB.getStoreInfoByStoreId(storeId)
-    
     let productInfo = await mysqlDB.getProductsAndImagesByStoreID(storeId)
     let storeImages = await mysqlDB.getShopPhotoByStoreId(storeId)
  
-
-
     res.render("follow_business/follow_business_2",{ storeInfo:storeInfo[0], productInfo:productInfo, storeImages:storeImages })
 })
 
 
-
-
-
-
 router.post("/follow_business_2", async (req, res) => {
 
-    //the function that adds to the th follower to number of followers
-    //the function that get the new followers quantity
+    // the function that adds to the th follower to number of followers
+    // the function that get the new followers quantity
 
     // let storeId = 1
     let followersNum = 3
